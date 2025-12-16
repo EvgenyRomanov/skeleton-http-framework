8498f320f8b3d7d8e88502af70b41f67bd3a8df8
EvgenyRomanov <e.romanov93@yandex.ru>
2025-12-16 17:43:40 +0500

    исправления psalm

--- bootstrap/di_bindings/migrations.php
+++ bootstrap/di_bindings/migrations.php
@@ -19,19 +19,43 @@ return static function (Container $container): void {
          *     } $config
          */
         $config = $configAggregator->getMergedConfig();
+        /** @var array{
+         *     application_name?: string,
+         *     charset?: string,
+         *     dbname?: string,
+         *     defaultTableOptions?: array<string, mixed>,
+         *     driver?: 'ibm_db2'|'mysqli'|'oci8'|'pdo_mysql'|'pdo_oci'|'pdo_pgsql'|'pdo_sqlite'|'pdo_sqlsrv'|'pgsql'|'sqlite3'|'sqlsrv',
+         *     driverClass?: class-string<Doctrine\DBAL\Driver>,
+         *     driverOptions?: array<array-key, mixed>,
+         *     host?: string,
+         *     keepReplica?: bool,
+         *     memory?: bool,
+         *     password?: string,
+         *     path?: string,
+         *     persistent?: bool,
+         *     port?: int,
+         *     primary?: array{application_name?: string, charset?: string, dbname?: string, defaultTableOptions?: array<string, mixed>, driver?: 'ibm_db2'|'mysqli'|'oci8'|'pdo_mysql'|'pdo_oci'|'pdo_pgsql'|'pdo_sqlite'|'pdo_sqlsrv'|'pgsql'|'sqlite3'|'sqlsrv', driverClass?: class-string<Doctrine\DBAL\Driver>, driverOptions?: array<array-key, mixed>, host?: string, memory?: bool, password?: string, path?: string, persistent?: bool, port?: int, serverVersion?: string, sessionMode?: int, unix_socket?: string, user?: string, wrapperClass?: class-string<Doctrine\DBAL\Connection>},
+         *     replica?: array<array-key, array{application_name?: string, charset?: string, dbname?: string, defaultTableOptions?: array<string, mixed>, driver?: 'ibm_db2'|'mysqli'|'oci8'|'pdo_mysql'|'pdo_oci'|'pdo_pgsql'|'pdo_sqlite'|'pdo_sqlsrv'|'pgsql'|'sqlite3'|'sqlsrv', driverClass?: class-string<Doctrine\DBAL\Driver>, driverOptions?: array<array-key, mixed>, host?: string, memory?: bool, password?: string, path?: string, persistent?: bool, port?: int, serverVersion?: string, sessionMode?: int, unix_socket?: string, user?: string, wrapperClass?: class-string<Doctrine\DBAL\Connection>}>,
+         *     serverVersion?: string,
+         *     sessionMode?: int,
+         *     unix_socket?: string,
+         *     user?: string,
+         *     wrapperClass?: class-string<Doctrine\DBAL\Connection>
+         *  } $params */
+        $params = [
+            'dbname' => $config['db']['database'],
+            'user' => $config['db']['username'],
+            'password' => $config['db']['password'],
+            'host' => $config['db']['host'],
+            'driver' => $config['db']['driver'],
+            'charset' => 'utf8',
+            'port' => $config['db']['port'],
+            'schema' => 'public',
+        ];
 
         return DependencyFactory::fromConnection(
             new ConfigurationArray($config['migrations']),
-            new ExistingConnection(DriverManager::getConnection([
-                'dbname' => $config['db']['database'],
-                'user' => $config['db']['username'],
-                'password' => $config['db']['password'],
-                'host' => $config['db']['host'],
-                'driver' => $config['db']['driver'],
-                'charset' => 'utf8',
-                'port' => $config['db']['port'],
-                'schema' => 'public',
-            ]))
+            new ExistingConnection(DriverManager::getConnection($params))
         );
     });
 
